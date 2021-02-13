8f147dfc26dd50a9175f2d4e44d957b8fbb6fe76
Lucas Pereira <pereira.lucas23@gmail.com>
2021-02-12 21:12:02 -0300

    Adicionando tabela de funcionários

---
+++ tb_func.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM tb_func
+LIMIT 0, 1000
+
+-- Date: 2021-02-12 21:10
+*/
+INSERT INTO `` (`id`,`nome`,`matricula`,`admissao`,`cargo`,`departamento`,`superior`,`salario`) VALUES (1,'Lucas Silva Pereira',1,'2021-01-14','Desenvolvedor Jr ','Tecnologia','Alessandro Nunes',6500);
+INSERT INTO `` (`id`,`nome`,`matricula`,`admissao`,`cargo`,`departamento`,`superior`,`salario`) VALUES (2,'Rafael de Oliveira',2,'2019-02-15','Desenvolvedor Jr','Tecnologia','Alessandro Nunes',6500);
+INSERT INTO `` (`id`,`nome`,`matricula`,`admissao`,`cargo`,`departamento`,`superior`,`salario`) VALUES (3,'Vagner Monteiro',3,'2018-03-30','Desenvolvedor Pl','Tecnologia','Alessandro Nunes',8200);
+INSERT INTO `` (`id`,`nome`,`matricula`,`admissao`,`cargo`,`departamento`,`superior`,`salario`) VALUES (4,'Rodrigo de Oliveira',4,'2015-02-25','Desenvolvedor Jr','Tecnologia','Alessandro Nunes',8200);
+INSERT INTO `` (`id`,`nome`,`matricula`,`admissao`,`cargo`,`departamento`,`superior`,`salario`) VALUES (5,'Lucas Rodrigues',5,'2014-02-14','Scrum MasteR','Tecnologia','Pablo Rodrigues',9000);
